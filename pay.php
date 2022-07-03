4bc4f084263aba7fff9448ffd290510910222613
puzpa <69891394+puzpa@users.noreply.github.com>
2022-07-03 10:05:42 +0800

    Add files via upload

---
+++ pay.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+
+
+
+
+?>
+<html>
+
+<from name='Spgateway' method='post action='https://cwww.newebpay.com'> 
+
+<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
+
+<input type='submit' value='Submit'>
+
+</form>
+</html>
