9fc235c8caef397fc8fbdce42219206c79d188f0
puzpa <69891394+puzpa@users.noreply.github.com>
2022-07-03 09:37:56 +0800

    Update pay.php

--- pay.php
+++ pay.php
@@ -9,14 +9,14 @@
 ?>
 <html>
 
-<from name='Spgateway' method='post action='https://cwww.newebpay.com'> 
+<from name='Spgateway' method='post action='https://core.spgateway.com/MPG/mpg_gateway'> 
 
-<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
-<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
-<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
-<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
-<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
-<input type='hidden' id='MerchantID' name='MerchantID' value=''><br>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''>
 
 <input type='submit' value='Submit'>
 
