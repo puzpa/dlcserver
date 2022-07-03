0ec25da23d09798d46a882b5dc0f1cce6ec39bce
puzpa <69891394+puzpa@users.noreply.github.com>
2022-07-03 15:04:15 +0800

    Update and rename pay.html to pay.php

---
+++ pay.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+
+
+
+?>
+<html>
+
+<from name='Spgateway' method='post' action='https://cwww.newebpay.com'> 
+
+<input type='hidden' id='MerchantID' name='MerchantID' value='MS1609378169'>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''>
+<input type='hidden' id='MerchantID' name='MerchantID' value=''>
+
+<input type='submit' value='Submit'>
+
+</form>
+  
+  
+</html>
